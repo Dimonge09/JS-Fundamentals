a50ce8b7ed061325ce28950a11c04698b9dd16c6
Dimonge09 <dimongejeremy@gmail.com>
2025-07-12 15:00:40 +0000

    Add loop-based multi-languages message printer

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,2 +1,12 @@
 const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 console.log(messages.join("\n"));
+
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let i = 0;
+let result = "";
+
+for (; i < messages.length; i++) {
+  result += messages[i] + (i < messages.length - 1 ? "\n" : "");
+}
+
+console.log(result);
